6d3628a6d80bf26ac9f5a11d31e7cc5ba002d164
Kazuya Ito <tamaco601@gmail.com>
2020-08-04 22:55:12 +0900

    Initial Commit

---
+++ BurstMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BurstMemo
+//
+//  Created by 伊藤和也 on 2020/08/04.
+//  Copyright © 2020 kazuya ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
